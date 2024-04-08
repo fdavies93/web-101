e78c2bbc4731f120e5685dfa2aebd197ba763093
Frank Davies <fdavies93@gmail.com>
2024-04-08 19:46:17 +0800

    Add sieve of erastothenes solution

---
+++ examples/01-javascript/assignment-sieve/sieve.js
@@ -0,0 +1,19 @@
+function sieve(n) {
+  let currentList = [];
+  for (let i = 2; i <= n; i++) currentList.push(i)
+
+  let i = 0
+  let num;
+  // we know it will never remove something earlier in the list
+  // therefore it's safe to use currentList[i]
+  while (i < currentList.length) {
+    num = currentList[i]
+    currentList = currentList.filter( (el) => (el == num || (el % num) != 0))
+    i++
+  }
+  return currentList
+}
+
+const primes = sieve(100)
+
+console.log(primes)
